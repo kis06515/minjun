7c70e6b4321da5cca04eb172daf1dbd4d2afa732
kis06515 <53353719+kis06515@users.noreply.github.com>
2024-11-11 16:16:44 +0900

    cloud
    실습

---
+++ src/test/java/kr/ac/gwangju/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.gwangju.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
